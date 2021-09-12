d80d6ac76c8d14ed9010fae1763cab9c32c5df18
Leo Ramos Andrade <leo.ramos182@hotmail.com>
2021-09-12 17:46:35 -0300

    feito peojeto copia do whatsapp

---
+++ whatscopia/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import react from 'react';
+import SecaoEnviarMensagem from './components/SecaoEnviarMensagem/SecaoEnviarMensagem'
+
+function App() {
+  return (
+    <div className="App">
+      <SecaoEnviarMensagem></SecaoEnviarMensagem>
+    </div>
+  );
+}
+
+export default App;
